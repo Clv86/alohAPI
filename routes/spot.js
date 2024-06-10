a7c0a38b13b408bdfb536e61dc716621a69e0f42
Cl <charles-louis.velieu@tutanota.com>
2024-06-10 21:44:22 +0200

    Spot CRUD added

---
+++ routes/spot.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const spotCtrl = require('../controllers/spot');
+
+router.post('/add', spotCtrl.addSpot);
+router.get('/', spotCtrl.getAllSpots);
+router.get('/:name', spotCtrl.getOneSpot);
+router.put('/:id', spotCtrl.modifySpot);
+router.delete('/:id', spotCtrl.deleteSpot);
+
+module.exports = router;
