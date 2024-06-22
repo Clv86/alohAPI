d138c5f4f5b831fffed93633df9d84794c2e8fdc
Clv86 <charles-louis.velieu@tutanota.com>
2024-06-22 15:19:42 +0200

    Rewrote with Async/await method

--- server.js
+++ server.js
@@ -1,34 +1,42 @@
-const express = require('express')
-const app = express()
-const PORT = 8080
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const mongoose = require('mongoose');
 
-require('dotenv').config()
+require('dotenv').config();
 
-const spotRoutes = require('./routes/spot')
-const dayConditionsRoutes = require('./routes/dayConditions')
-const weekConditionsRoutes = require('./routes/weekConditions')
+const spotRoutes = require('./routes/spot');
+const dayConditionsRoutes = require('./routes/dayConditions');
+const weekConditionsRoutes = require('./routes/weekConditions');
 
-mongoose
-    .connect(process.env.URL)
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'))
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.URL);
+        console.log('Connexion à MongoDB réussie !');
+    } catch (error) {
+        console.error('Connexion à MongoDB échouée !', error);
+    }
+};
 
-app.use(express.json())
+connectToDatabase();
+
+app.use(express.json());
+
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
-    )
+    );
     res.setHeader(
         'Access-Control-Allow-Methods',
         'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-    )
-    next()
-})
-app.use('/spot', spotRoutes)
-app.use('/dayConditions', dayConditionsRoutes)
-app.use('/weekConditions', weekConditionsRoutes)
+    );
+    next();
+});
 
-app.listen(PORT, () => console.log(`it's alive on http://localhost:${PORT}`))
+app.use('/spot', spotRoutes);
+app.use('/dayConditions', dayConditionsRoutes);
+app.use('/weekConditions', weekConditionsRoutes);
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
