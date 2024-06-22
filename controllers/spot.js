b9e90fc80352f3eb5760e2c00800c048353a1dc1
Clv86 <charles-louis.velieu@tutanota.com>
2024-06-22 14:35:05 +0200

    Rewrite with async/await method

--- controllers/spot.js
+++ controllers/spot.js
@@ -1,41 +1,57 @@
-const Spot = require('../models/spot')
+const Spot = require('../models/spot');
 
-exports.addSpot = (req, res, next) => {
-    delete req.body._id
-    const spot = new Spot({
-        name: req.body.name,
-        coordinates: {
-            latitude: req.body.coordinates.latitude,
-            longitude: req.body.coordinates.longitude,
-        },
-    })
-    spot.save()
-        .then(() => res.status(201).json({ message: 'Spot enregistré !' }))
-        .catch((error) => res.status(400).json({ error }))
-}
-exports.getAllSpots = (req, res, next) => {
-    Spot.find()
-        .then((spots) => {
-            res.status(200).json(spots)
-        })
-        .catch((error) => res.status(400).json({ error }))
-}
-exports.getOneSpot = (req, res, next) => {
-    Spot.findOne({
-        name: req.params.name,
-    })
-        .then((spot) => {
-            res.status(200).json(spot)
-        })
-        .catch((error) => res.status(404).json({ error }))
-}
-exports.modifySpot = (req, res, next) => {
-    Spot.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Spot modifié' }))
-        .catch((error) => res.status(400).json({ error }))
-}
-exports.deleteSpot = (req, res, next) => {
-    Spot.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Spot supprimé' }))
-        .catch((error) => res.status(400).json({ error }))
-}
+exports.addSpot = async (req, res, next) => {
+    try {
+        delete req.body._id;
+        const spot = new Spot({
+            name: req.body.name,
+            coordinates: {
+                latitude: req.body.coordinates.latitude,
+                longitude: req.body.coordinates.longitude,
+            },
+        });
+        await spot.save();
+        res.status(201).json({ message: 'Spot enregistré !' });
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+};
+
+exports.getAllSpots = async (req, res, next) => {
+    try {
+        const spots = await Spot.find();
+        res.status(200).json(spots);
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+};
+
+exports.getOneSpot = async (req, res, next) => {
+    try {
+        const spot = await Spot.findOne({ name: req.params.name });
+        if (!spot) {
+            return res.status(404).json({ error: 'Spot non trouvé' });
+        }
+        res.status(200).json(spot);
+    } catch (error) {
+        res.status(404).json({ error });
+    }
+};
+
+exports.modifySpot = async (req, res, next) => {
+    try {
+        await Spot.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id });
+        res.status(200).json({ message: 'Spot modifié' });
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+};
+
+exports.deleteSpot = async (req, res, next) => {
+    try {
+        await Spot.deleteOne({ _id: req.params.id });
+        res.status(200).json({ message: 'Spot supprimé' });
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+};
